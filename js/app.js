c44cc11813d05f22494cc5025ab51586d9a5974b
edwardbryant <edward.bryant@gmail.com>
2015-02-02 07:52:16 -0600

    changes

--- js/app.js
+++ js/app.js
@@ -2,11 +2,42 @@
 
 $(document).ready( function() {
 
-    $.getJSON('http://www.omdbapi.com/?s=Star%20Wars&r=json', function(data){
-
-        var result = data.Search[0].Title;
-        console.log(result);
-
+    $('button').click(function() {               
+        var query_term = $('#query').val();
+        requestData(query_term);
     });
 
+    $(document).keydown(function(e) {
+        if (e.keyCode == 13) {
+            var query_term = $('#query').val();
+            requestData(query_term);
+        }   
+    })
+
 });
+
+function requestData(query_term) {
+    var params = {
+        s: query_term,
+        r: 'json'
+    };
+    var url = 'http://www.omdbapi.com';
+    $.getJSON(url, params, function(data){
+        showResults(data.Search);
+    });
+}
+function showResults(data) {
+    var results = "";
+    $.each(data, function(index,value){
+        if (value.Type == "movie") {
+            var icon = "<i class=\"fa fa-film\"></i>";
+        } else if (value.Type == "series") {
+            var icon = "<i class=\"fa fa-desktop\"></i>";
+        } else {
+            var icon = "<i class=\"fa fa-square-o\"></i>";
+        }
+        results += "<div> " + icon + " &nbsp; <a href=\"http://www.imdb.com/title/" + value.imdbID + "/\" target=\"_blank\">" + value.Title + "</a></div>";
+    });    
+    var results_node = $(results);
+    $('#results').append(results_node);
+}
